1de008a299af188c9157bae9b0fee3dd2e810e28
git-marcopitra <marcomorete73@hotmail.com>
2023-12-29 02:01:18 +0100

    :bookmark: feat: new alpha version

--- src/lib/firestore/index.ts
+++ src/lib/firestore/index.ts
@@ -1,3 +1,5 @@
 export { default as getAllData } from "./get-all-data";
 export { default as getPagination } from "./get-pagination";
 export { default as getCollection } from "./get-collection";
+export * from "./utils";
+export * from "./firestore.protocol";
