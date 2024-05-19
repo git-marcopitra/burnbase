32b85fd37a5eceea1042a676ae992bebbadb2d6b
git-marcopitra <marcomorete73@hotmail.com>
2024-05-20 00:37:06 +0100

    :bug: fix: delete file cuket ref

--- src/lib/storage/delete-file.ts
+++ src/lib/storage/delete-file.ts
@@ -1,6 +1,7 @@
 import { deleteObject, ref, getStorage } from "firebase/storage";
 import type { TDeleteObject } from "./storage.protocol";
+import { bucket } from "./storage.utils";
 
-const deleteFile: TDeleteObject = (url) => deleteObject(ref(getStorage(), url));
+const deleteFile: TDeleteObject = (url) => deleteObject(ref(bucket, url));
 
 export default deleteFile;
