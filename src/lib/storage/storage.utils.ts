9f03c33f90b3e6e6cc1a6052876b9f25d6bab2b1
git-marcopitra <marcomorete73@hotmail.com>
2024-07-08 10:12:50 +0100

    :bug: fix: base url for storage

--- src/lib/storage/storage.utils.ts
+++ src/lib/storage/storage.utils.ts
@@ -5,7 +5,7 @@ import type { TGetPathStorage } from "./storage.protocol";
 export const bucket = getStorage(app);
 
 export const getPathStorageFromUrl: TGetPathStorage = (url) => {
-  const baseUrl = `https://firebasestorage.googleapis.com/v0/b/${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}/o/`;
+  const baseUrl = `https://firebasestorage.googleapis.com/v0/b/${app.options.storageBucket}/o/`;
 
   const imagePath: string = url.replace(baseUrl, "").replace(/%2F/g, "/");
 
